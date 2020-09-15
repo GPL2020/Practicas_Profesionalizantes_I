8ad2bf66672535bf2bcac9d2d183840c17f495a7
GPL2020 <69278375+GPL2020@users.noreply.github.com>
2020-09-14 21:02:47 -0300

    Persistencia de Archivos

--- TP 02/Persistencia.cpp
+++ TP 02/Persistencia.cpp
@@ -29,19 +29,12 @@ int main()
     int tempNumber;
     FileData files[MAX_FILES];
 
+    numberOfFiles = levantarDatos("datos.bin", files);
+
     do {
         option = menu();
         switch (option) {
-            case 0: // Guardar el contenido del arreglo en un archivo
-                persistirDatos("datos.bin", files, numberOfFiles);
-                cout << "\nDatos guardados en el archivo (datos.bin)... " << endl;
-                break;
-            case 1: // Pasar datos del archivo a un arreglo
-                numberOfFiles = levantarDatos("datos.bin", files);
-                if (numberOfFiles > 0)
-                    cout << "\nDatos transferidos a la memoria.... " << endl;
-                break;
-            case 2: // Añadir un dato nuevo
+            case 1: // Añadir un dato nuevo
                 if (numberOfFiles < MAX_FILES) {
                     cout << "\nIntroduce el nombre del archivo: ";
                     cin.ignore();
@@ -53,14 +46,14 @@ int main()
                 else
                     cout << "\nMáximo de archivos alcanzado (1000)!" << endl;
                 break;
-            case 3: // Mostrar todos
+            case 2: // Mostrar todos
                 cout << endl;
                 for (i = 0; i < numberOfFiles; i++)
                     cout << "Nombre: " << files[i].name
                          << "; Tamaño: " << files[i].size
                          << "Kb\n" << endl;
                 break;
-            case 4: // Mostrar según el tamaño
+            case 3: // Mostrar según el tamaño
                 cout << "\n¿A partir de que tamaño quieres que te muestre? ";
                 cin >> tempNumber;
                 for (i = 0; i < numberOfFiles; i++)
@@ -69,7 +62,7 @@ int main()
                              << "; Tamaño: " << files[i].size
                              << "Kb\n" << endl;
                 break;
-            case 5: // Ver todos los datos (pocos) de un archivo
+            case 4: // Ver todos los datos (pocos) de un archivo
                 cout << "\n¿De que archivo quieres ver todos los datos? ";
                 cin.ignore();
                 getline(cin, tempText);
@@ -79,14 +72,17 @@ int main()
                              << "; Tamaño: " << files[i].size
                              << "Kb\n" << endl;
                 break;
-            case 6: // Salir: avisamos que salimos
+            case 5: // Salir: avisamos que salimos
                 cout << "\nFin del programa" << endl;
                 break;
             default: // Otra opción: no válida
                 cout << "\n¡Opcion desconocida!" << endl;
                 break;
         }
-    } while (option != 6); // Si la opción es 5, terminamos
+    } while (option != 5); // Si la opción es 5, terminamos
+
+    persistirDatos("datos.bin", files, numberOfFiles);
+    cout << "\nDatos guardados en el archivo (datos.bin)... " << endl;
 
     cout << "\nPresione ENTER para finalizar... ";
     cin.sync();
@@ -97,14 +93,13 @@ int main()
 
 int menu() {
     int op;
+
     cout << endl;
-    cout << "0.- Persistir datos" << endl;
-    cout << "1.- Levantar datos del archivo" << endl;
-    cout << "2.- Añadir datos de un nuevo archivo" << endl;
-    cout << "3.- Mostrar los nombres de todos los archivos" << endl;
-    cout << "4.- Mostrar archivos que sean de mas de un cierto tamaño" << endl;
-    cout << "5.- Ver datos de un archivo" << endl;
-    cout << "6.- Salir" << endl;
+    cout << "1.- Añadir datos de un nuevo archivo" << endl;
+    cout << "2.- Mostrar los nombres de todos los archivos" << endl;
+    cout << "3.- Mostrar archivos que sean de mas de un cierto tamaño" << endl;
+    cout << "4.- Ver datos de un archivo" << endl;
+    cout << "5.- Salir" << endl;
     cout << "Escoja una opcion: ";
     cin >> op;
 
@@ -118,6 +113,7 @@ void persistirDatos(string nombreArchivo, FileData f[MAX_FILES], int n) {
     if (arch.is_open()) {
         if (n > 0)
             for (int i = 0; i < n; i++)
+
                 arch.write((char *) &f[i], sizeof(FileData));
         else
             cout << "\nNo hay datos para persisitir... " << endl;
@@ -129,7 +125,7 @@ void persistirDatos(string nombreArchivo, FileData f[MAX_FILES], int n) {
 
 // Pasa los datos de un archivo binario a un arreglo
 int levantarDatos(string nombreArchivo, FileData f[MAX_FILES]) {
-    ifstream arch(nombreArchivo, ios::app);
+    ifstream arch(nombreArchivo, ios::binary);
     int i = 0;
 
     if (arch.is_open()) {
