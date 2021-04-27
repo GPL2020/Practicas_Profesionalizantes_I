6bb3cd19b663e033c04467d50acd4a11cd18f3d8
GPL2020 <gplatta@gmail.com>
2021-04-27 20:44:52 -0300

    Trabajo Práctico Final - Sistema para tomar asistencia (POO)

--- TP Final_Assistance/build.bat
+++ TP Final_Assistance/build.bat
@@ -8,7 +8,7 @@ g++ -Wall -std=c++11 -c StudentXCourseManager.cpp
 g++ -Wall -std=c++11 -c AssistanceManager.cpp
 cd ..
 g++ -Wall -std=c++11 -c main.cpp 
-g++ ./src/Course.o ./src/Student.o ./src/CourseMapper.o ./src/StudentMapper.o ./src/StudentXCourseManager.o ./src/AssistanceManager main.o -o Assistance.exe
+g++ ./src/Course.o ./src/Student.o ./src/CourseMapper.o ./src/StudentMapper.o ./src/StudentXCourseManager.o ./src/AssistanceManager.o main.o -o Assistance.exe
 
 echo "Programa compilado"
 pause
